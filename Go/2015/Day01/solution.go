0b1520bd7df87a494600bf1fae989b537865bb74
MaibHassin <maibhassin@gmail.com>
2025-05-23 16:50:49 +0200

    feat(2015-go-day01): Solve Part 2

--- Go/2015/Day01/solution.go
+++ Go/2015/Day01/solution.go
@@ -9,19 +9,37 @@ import (
 func main() {
 	input := readInput()
 
-	timesGoUp := strings.Count(input, "(")
-	timesGoDown := strings.Count(input, ")")
+	// timesGoUp := strings.Count(input, "(")
+	// timesGoDown := strings.Count(input, ")")
 
-	finalFloor := timesGoUp - timesGoDown
+	// finalFloor := timesGoUp - timesGoDown
+	finalFloor := 0
+
+	firstNeagtiveFound := false
+	var firstNegative int
+
+	for i, floor := range strings.Split(input, "") {
+		if floor == "(" {
+			finalFloor++
+		} else {
+			finalFloor--
+		}
+
+		if finalFloor < 0 && !firstNeagtiveFound {
+			firstNegative = i + 1
+			firstNeagtiveFound = true
+		}
+	}
 
 	fmt.Printf("Santa ends up on: %d\n", finalFloor)
+	fmt.Printf("First position when santa enters the basement: %d\n", firstNegative)
 }
 
 func readInput() string {
 	f, err := os.ReadFile("input.txt")
 	if err != nil {
 		fmt.Println("Unable to read file")
-		return "ERROR"
+		os.Exit(1)
 	}
 
 	return string(f)
