86255b68fc1af6ddcd6aecaa7082829102895d5a
MaibHassin <maibhassin@gmail.com>
2025-05-23 17:27:11 +0200

    feat(go): Create Go starter file

---
+++ Go/solution.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	inputFileName := flag.String("input", "input.txt", "Path to the input file")
+	flag.Parse()
+
+	input, err := readInput(*inputFileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	_ = input //remove this line
+}
+
+func readInput(filename string) (string, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return "", fmt.Errorf("Failed to read file '%s': %w", filename, err)
+	}
+
+	content := strings.TrimSpace(string(data))
+	return content, nil
+}
